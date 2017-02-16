6fb61a1f9ee9638fe1ccd35a6c756b328ac55007
eccentricayman <aahmed3@stuy.edu>
2017-02-15 20:52:17 -0500

    airquality

---
+++ AirQuality2/AirQuality2.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include "plotly_streaming_ethernet.h"
+
+//Sensor Setup
+#define airquality_sensor_pin 1
+#define gas_sensor_pin 1
+
+// Sign up to plotly here: https://plot.ly
+// View your API key and streamtokens here: https://plot.ly/settings
+#define nTraces 2
+// View your tokens here: https://plot.ly/settings
+// Supply as many tokens as data traces
+// e.g. if you want to ploty A0 and A1 vs time, supply two tokens
+char *tokens[nTraces] = {"25tm9197rz", "unbi52ww8a"};
+// arguments: username, api key, streaming token, filename
+plotly graph("workshop", "v6w5xlbx9j", tokens, "filename", nTraces);
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte my_ip[] = { 199, 168, 222, 18 }; // google will tell you: "public ip address"
+
+void startEthernet(){
+    Serial.println("... Initializing ethernet");
+    if(Ethernet.begin(mac) == 0){
+        Serial.println("... Failed to configure Ethernet using DHCP");
+        // no point in carrying on, so do nothing forevermore:
+        // try to congifure using IP address instead of DHCP:
+        Ethernet.begin(mac, my_ip);
+    }
+    Serial.println("... Done initializing ethernet");
+    delay(1000);
+}
+
+
+void setup() {
+  graph.maxpoints = 100;
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  startEthernet();
+
+  bool success;
+  success = graph.init();
+  if(!success){while(true){}}
+  graph.openStream();
+}
+
+void loop() {
+    int airquality_value = analogRead(airquality_sensor_pin);
+    int gas_value = analogRead(gas_sensor_pin);
+    float volume = (float)gas_value/1024*5.0*1000;
+    graph.plot(millis(), airquality_value, tokens[0]);
+    graph.plot(millis(), volume, tokens[1]);
+    delay(50);
+}
